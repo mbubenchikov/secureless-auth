7eea8cf76472515ae847164eb8fa105d77e23551
Maxim Bubenchikov <mbubenchikov0@gmail.com>
2026-04-13 18:54:53 +0400

    open id connect server base

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/mbubenchikov/secureless-auth
 
 go 1.26.2
+
+require (
+	github.com/go-jose/go-jose/v4 v4.1.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/luikyv/go-oidc v0.17.0 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+	"net/http"
+
+	"github.com/luikyv/go-oidc/pkg/goidc"
+	"github.com/luikyv/go-oidc/pkg/provider"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	jwks := goidc.JSONWebKeySet{
+		Keys: []goidc.JSONWebKey{{
+			KeyID:     "key_id",
+			Key:       key,
+			Algorithm: "RS256",
+		}},
+	}
+
+	op, _ := provider.New(
+		goidc.ProfileOpenID,
+		"http://localhost",
+		func(_ context.Context) (goidc.JSONWebKeySet, error) {
+			return jwks, nil
+		},
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", op.Handler())
+
+	server := &http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
 }
